e74463f2697e19b48db213cea05895bee4020257
Tomasz Kubiak <tomasz.m.kubiak@icloud.com>
2025-02-14 00:23:47 +0100

    Add stub cpu metric collector

--- Sources/SysMonitor/MetricCollecting.swift
+++ Sources/SysMonitor/MetricCollecting.swift
@@ -1,3 +1,13 @@
 protocol MetricCollecting {
     func collect() -> UsagePercentageable
 }
+
+protocol CpuMetricCollecting {
+    func collect() -> Double
+}
+
+class CpuMetricCollector: CpuMetricCollecting {
+    func collect() -> Double {
+        .zero
+    }
+}
